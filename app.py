4b10e11c5a3861b2d13909951589852c188a78ab
BALAJANANI05 <balajanani180@gmail.com>
2026-02-28 13:04:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ trusted_sources = [
 
 # Google Search API settings
 # Replace with your actual API key and Search Engine ID
-API_KEY = 'AIzaSyCLA4qNgPpuU24KA3ndi9EU482nA7f4h54' # Replace with your actual API key
+API_KEY = 'AIzaSyBL1dzP3NsbrL01vnNBFfmLa3Whp3d0GPA' # Replace with your actual API key
 SEARCH_ENGINE_ID = '37002a679147b437b' # Replace with your actual Search Engine ID
 
 import time
